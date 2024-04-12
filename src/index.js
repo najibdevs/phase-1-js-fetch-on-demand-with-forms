ff35d29988f07eb7f8ebce6ad04de9e4240004d1
najibdevs <najibsoftwareeng@gmail.com>
2024-04-12 21:41:35 +0300

    fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const init = () => {
+    const form = document.querySelector("form");
+    const movieDetails = document.getElementById("movieDetails");
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+  
+      const input = document.getElementById("searchByID").value;
+      const movieId = parseInt(input); // Convert input to an integer
+  
+      try {
+        const response = await fetch(`http://localhost:3000/movies/${movieId}`);
+        if (!response.ok) {
+          throw new Error("Movie not found");
+        }
+        const movie = await response.json();
+        displayMovieDetails(movie);
+      } catch (error) {
+        movieDetails.innerHTML = `<p>${error.message}</p>`;
+      }
+    });
+  
+    function displayMovieDetails(movie) {
+      const title = document.querySelector("#movieDetails h4");
+      const summary = document.querySelector("#movieDetails p");
+  
+      title.textContent = movie.title;
+      summary.textContent = movie.summary;
+    }
+  };
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
